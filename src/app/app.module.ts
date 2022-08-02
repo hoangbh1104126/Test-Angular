bb6fe65897a0bcc374442cc070a55105accdabf0
hoangbh1104126 <110478946+hoangbh1104126@users.noreply.github.com>
2022-08-02 22:24:25 +0700

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component'
+import { HelloAdminComponent } from './helloAdmin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    HelloAdminComponent,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
